e7308b742acf7df72add19e46e4e53a5f0e08403
KK <bebound@gmail.com>
2018-11-02 23:20:27 +0800

    ?

--- plugins/orgmode/conf.el
+++ plugins/orgmode/conf.el
@@ -1,4 +1,6 @@
-(let ((default-directory "~/.emacs.d/elpa/27.0/develop/"))
-  (normal-top-level-add-subdirs-to-load-path))
+(setq dir "~/.emacs.d/elpa/27.0/develop/")
+(if(file-directory-p dir)
+    (let ((default-directory dir))
+      (normal-top-level-add-subdirs-to-load-path)))
 (add-to-list 'load-path "~/emacs-htmlize/")
 (require 'htmlize)
